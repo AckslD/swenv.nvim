28f0a8f0a6875f75a956e6a52d64036abbda91ef
palandovalex <palandovalex@gmail.com>
2024-03-05 00:22:41 +0300

    fix os relation

--- lua/swenv/project.lua
+++ lua/swenv/project.lua
@@ -1,21 +1,16 @@
 local M = {}
-local Path = require('plenary.path')
+-- local Path = require('plenary.path')
 
 local function isdir(dir_path)
-  local f = io.popen('[ -d "' .. dir_path .. '" ] && echo -n y')
-  local result = f:read(1)
-  f:close()
-  return result == "y"
+  return vim.fn.isdirectory(dir_path) ~= 0
 end
 
 local function get_local_env(venv_path)
   local venv_cfg = io.open(venv_path .. '/pyvenv.cfg', 'r')
   if not venv_cfg then
-    print('no file: ' .. venv_path .. '/pyvenv.cfg')
     return nil
   end
 
-  
   local configText = venv_cfg:read('*a') -- *a or *all reads the whole file
   local pattern = "prompt%s*=%s*([%w_]+)"
   local venv_name = configText:match(pattern)
@@ -30,6 +25,7 @@ end
 
 -- Get the name from a `.venv` file in the project root directory.
 M.get_project_venv_data = function(project_dir)
+  print('get_project_venv_data')
   local venv_path = project_dir .. '/.venv'
   local file = io.open(venv_path, 'r') -- r read mode
   if isdir(venv_path) then
@@ -41,8 +37,6 @@ M.get_project_venv_data = function(project_dir)
     end
     return nil
   end
-  print('isdir for ' .. venv_path .. ' is ' .. tostring(isdir(venv_path)))
-  
   local content = file:read('*a') -- *a or *all reads the whole file
   file:close()
   return content:match('^%s*(.-)%s*$') -- Trim whitespace
