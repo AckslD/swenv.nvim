db2b40d4fa719f8dda5cbd1708d16d90e03919a2
palandovalex <palandovalex@gmail.com>
2024-03-04 21:35:14 +0300

    add local .venv dirs support

--- lua/swenv/project.lua
+++ lua/swenv/project.lua
@@ -1,9 +1,37 @@
 local M = {}
+local Path = require('plenary.path')
+
+
+local function isdir(path)
+    return Path.new(path).is_dir()
+end
+
+local function get_local_env(venv_path)
+  local venv_cfg = io.open(venv_path .. '/pyvenv.cfg', 'r')
+  if not venv_cfg then
+    return nil
+  end
+
+  local configText = venv_cfg:read('*a') -- *a or *all reads the whole file
+  local pattern = "prompt%s*=%s*([%w_]+)"
+  local venv_name = configText:match(pattern)
+
+  local venv = {
+    name = venv_name,
+    path = Path:new(venv_path),
+    source = 'local',
+  }
+  return venv
+end
 
 -- Get the name from a `.venv` file in the project root directory.
-M.read_venv_name = function(project_dir)
-  local file = io.open(project_dir .. '/.venv', 'r') -- r read mode
+M.get_project_venv_data = function(project_dir)
+  local venv_path = project_dir .. '/.venv'
+  local file = io.open(venv_path, 'r') -- r read mode
   if not file then
+    if isdir(venv_path)then
+      return get_local_env(venv_path)
+    end
     return nil
   end
   local content = file:read('*a') -- *a or *all reads the whole file
